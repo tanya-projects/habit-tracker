4af854ff11de608657e7c406a7a13bdd6d364d06
Tanya <106194959+tanya95gordeeva@users.noreply.github.com>
2022-12-26 00:03:31 +0400

    fix dates error in track

--- src/components/Helpers/UpdateTrackTillToday.js
+++ src/components/Helpers/UpdateTrackTillToday.js
@@ -36,67 +36,65 @@ dayjs.extend(relativeTime);
 //   }
 // }
 
-// function UpdateTrackTill(habit) {
-//   // Count difference in days from start date of tracking habit till today
-//   const daysFromStart = dayjs().diff(dayjs(habit.startDate), 'day');
-//   let testdatesarray = [];
-//   for (let i = 0; i < daysFromStart + 2; i++) {
-//     const dateTest = dayjs(habit.startDate).add(i, 'day');
-//     testdatesarray.push(dateTest);
-//   }
-//   console.log(daysFromStart, testdatesarray);
-
-//   // Find track with today date
-//   const todayTrack = habit.track.find(
-//     (track) => FormatDate(track.day) === FormatDate(dayjs())
-//   );
-//   console.log('Today track', todayTrack);
-
-//   // based on if we have today track or we just started to track habit (have 0 days from start date) we don't update habit track
-//   if (!daysFromStart || todayTrack) {
-//     return habit.track;
-//   } else {
-//     let updatedTrack = [];
-//     // based on days from start date we look for every date that should be in track
-//     for (let day = 0; day < daysFromStart + 2; day++) {
-//       const dayDate = dayjs().subtract(day, 'day');
-//       const checkDayTrack = habit.track.find(
-//         (track) => FormatDate(track.day) === FormatDate(dayDate)
-//       );
-//       // if we have this date in track, we add this date track
-//       if (checkDayTrack) {
-//         updatedTrack.push(checkDayTrack);
-//       } else {
-//         updatedTrack.push({ day: dayDate, isDone: null });
-//       }
-//     }
-
-//     const addTodayToArray = [...updatedTrack.reverse()];
-//     console.log('upd track & add array', updatedTrack, addTodayToArray);
-//     return addTodayToArray;
-//   }
-// }
-
 export default function UpdateTrackTillToday(habit) {
-  // Function to count difference in dates
-  const checkDatesDifference = () => {
-    const startTrackingDay = dayjs(habit.startDate);
-
-    console.log(startTrackingDay, dayjs());
-  };
-
-  checkDatesDifference();
-  // Count difference in DATES (not days) from start date of tracking habit till today
+  // Count difference in days from start date of tracking habit till today
   const daysFromStart = dayjs().diff(dayjs(habit.startDate), 'day');
-  console.log('Days from start', daysFromStart);
+  let testdatesarray = [];
+  for (let i = 0; i < daysFromStart + 1; i++) {
+    const dateTest = dayjs(habit.startDate).add(i, 'day');
+    testdatesarray.push(dateTest);
+  }
 
+  // Find track with today date
   const todayTrack = habit.track.find(
     (track) => FormatDate(track.day) === FormatDate(dayjs())
   );
-  if (todayTrack) {
+
+  // based on if we have today track or we just started to track habit (have 0 days from start date) we don't update habit track
+  if (!daysFromStart || todayTrack) {
     return habit.track;
   } else {
-    const addTodayToArray = [...habit.track, { day: dayjs(), isDone: null }];
+    let updatedTrack = [];
+    // based on days from start date we look for every date that should be in track
+    for (let day = 0; day < daysFromStart + 1; day++) {
+      const dayDate = dayjs().subtract(day, 'day');
+      const checkDayTrack = habit.track.find(
+        (track) => FormatDate(track.day) === FormatDate(dayDate)
+      );
+      // if we have this date in track, we add this date track
+      if (checkDayTrack) {
+        updatedTrack.push(checkDayTrack);
+      } else {
+        updatedTrack.push({ day: dayDate, isDone: null });
+      }
+    }
+
+    const addTodayToArray = [...updatedTrack.reverse()];
+
     return addTodayToArray;
   }
 }
+
+// export default function UpdateTrackTillToday(habit) {
+//   // Function to count difference in dates
+//   const checkDatesDifference = () => {
+//     const startTrackingDay = dayjs(habit.startDate);
+
+//     console.log(startTrackingDay, dayjs());
+//   };
+
+//   checkDatesDifference();
+//   // Count difference in DATES (not days) from start date of tracking habit till today
+//   const daysFromStart = dayjs().diff(dayjs(habit.startDate), 'day');
+//   console.log('Days from start', daysFromStart);
+
+//   const todayTrack = habit.track.find(
+//     (track) => FormatDate(track.day) === FormatDate(dayjs())
+//   );
+//   if (todayTrack) {
+//     return habit.track;
+//   } else {
+//     const addTodayToArray = [...habit.track, { day: dayjs(), isDone: null }];
+//     return addTodayToArray;
+//   }
+// }
